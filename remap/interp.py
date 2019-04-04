60bb3b017e4d812e98412b8d963d0a97434eb3af
EthanHolleman <eholleman@luc.edu>
2019-04-04 12:30:40 -0500

    Bug fixes in findSolos method

--- remap/interp.py
+++ remap/interp.py
@@ -132,24 +132,42 @@ def nameElements(mergedList, familyName):
 
 
 def findSolos(LTRList,completeCon,allowance):
+
     #create a new list that contains only the solo elements
         #takes first element and "reaches" with con seq to possible 2nd LTR
         ## TODO: not returning list correctly is empty
     soloList = []
     i = 0
-    while i < len(LTRList)-1:
-        print(i)
+    print(len(LTRList))
 
+    while i < len(LTRList):
+        print(i)
+        print("solos")
+
+        print(i)
         reach = LTRList[i].endLocation + completeCon
         diff = (LTRList[i+1].startLocation - reach) #need to make sure this is enough
+        print(diff)
+        #print(diff)
 
-        if(diff > allowance):
+        if(diff > -(completeCon) and diff <= 0): #test for truncated element
+            LTRList[i].status = "INTACT"
+            LTRList[i+1].status = "INTACT"
+            i+=1
+            if i==len(LTRList)-1: # breaks if next element is the last in the list
+                break
+        elif (abs(diff) > allowance):
             LTRList[i].status = "SOLO"
             soloList.append(LTRList[i])
+            if i == len(LTRList)-2: #breaks if second to last LTR is a solo, b/c next LTR must also be a solo
+                soloList.append(LTRList[i+1])
+                break
         else:
             LTRList[i].status = "INTACT"
             LTRList[i+1].status = "INTACT"
             i+=1
+            if i == len(LTRList)-1: # breaks if next element is the last in the list
+                break
 
         i+=1
 
